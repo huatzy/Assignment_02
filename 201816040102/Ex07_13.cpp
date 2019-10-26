a192a30b531718def9f6f23dac3b5fb9c2656321
WorleyBell <56495190+WorleyBell@users.noreply.github.com>
2019-10-26 23:53:10 +0800

    Create Ex07_13.cpp
    This is my assignment_02
    Student ID: 201816040102

---
+++ 201816040102/Ex07_13.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <array>
+#define M 20 
+using namespace std;
+ 
+int main()
+{
+	int num,i,j,k=0,flag=0;
+	array <int,M> a ;
+	for (i=0;i<M;i++)
+	{
+		cin >> num;
+		if(num >= 10 && num <= 100)		//判断数据是否处于10和100之间 
+		{
+			for(j=0;j<i;j++)
+			{
+				if(a[j]==num)			//判断num有没有和之前的数据重复 
+				{
+					flag=1;
+				}
+			} 
+			if(flag==0)					//若无重复，则将其写入数组 
+			{
+				a[k]=num;
+				k++;
+			}
+		}
+		flag=0;
+	}
+	for(i=0;i<k;i++)
+	{
+		cout << a[i] << endl;
+	} 
+}
