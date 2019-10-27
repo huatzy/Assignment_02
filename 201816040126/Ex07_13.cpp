3fdec25cb9cce0735d469b8d5a594008d0c104c9
9990403 <56550647+9990403@users.noreply.github.com>
2019-10-27 08:21:36 +0800

    Create Ex07_13.cpp

---
+++ 201816040126/Ex07_13.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	array< int,20 >s;
+	int m = 0,i,num;
+
+    cout << "请输入20个数字:\n" << endl;
+
+	for (i = 0; i < 20; i++)
+	{
+		cin >> num;//输入数字
+
+		if (num < 10 || num > 100)//判断输入数字正确性
+		{
+			cout << "输入数字错误" << endl;
+			continue;
+		}
+		int j = 0;
+		for (j = 0; j < m; j++) {
+			if (s[j] == num)
+				break;//找到相同的退出循环
+		}
+		if (j == m){
+			s[m] = num;
+			m++;
+		}
+	}
+
+    cout << "\n消除重复数字后，剩余数字为：\n" << endl;
+
+	for (int i = 0; i < m; ++i)//输出这串数字
+	{
+		cout << s[i] << " ";
+	}
+	return 0;
+}
